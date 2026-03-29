5d10294f443890d8de18cd7884040393893c21dc
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-30 00:45:59 +0100

    creer le model user et definit les champs

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,22 +4,19 @@ namespace App\Models;
 
 use Database\Factories\UtilisateurFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Utilisateur extends Authenticatable
+class User extends Authenticatable
 {
     /** @use HasFactory<UtilisateurFactory> */
     use HasFactory, Notifiable;
 
-    protected $table = 'utilisateurs';
+    protected $table = 'users';
 
     protected $fillable = [
         'nom',
         'prenom',
-        'nom_famille',
         'email',
         'telephone',
         'role',
@@ -51,56 +48,56 @@ class Utilisateur extends Authenticatable
         ];
     }
 
-    public function familles(): BelongsToMany
+    public function familles()
     {
         return $this->belongsToMany(Famille::class, 'famille_utilisateur')
             ->withPivot(['role', 'est_contact_principal', 'invite_le', 'accepte_le'])
             ->withTimestamps();
     }
 
-    public function famillesCreees(): HasMany
+    public function famillesCreees()
     {
         return $this->hasMany(Famille::class, 'cree_par');
     }
 
-    public function tachesCreees(): HasMany
+    public function tachesCreees()
     {
         return $this->hasMany(Tache::class, 'creee_par');
     }
 
-    public function tachesAttribuees(): HasMany
+    public function tachesAttribuees()
     {
         return $this->hasMany(Tache::class, 'attribuee_a');
     }
 
-    public function suivisTaches(): HasMany
+    public function suivisTaches()
     {
         return $this->hasMany(SuiviTache::class, 'acteur_id');
     }
 
-    public function conversations(): BelongsToMany
+    public function conversations()
     {
         return $this->belongsToMany(Conversation::class, 'participants_conversation')
             ->withPivot(['rejoint_a', 'dernier_lu_a', 'archive_a', 'est_muet'])
             ->withTimestamps();
     }
 
-    public function messages(): HasMany
+    public function messages()
     {
         return $this->hasMany(Message::class, 'expediteur_id');
     }
 
-    public function piecesJointesTeleversees(): HasMany
+    public function Attachments()
     {
         return $this->hasMany(PieceJointe::class, 'televerse_par');
     }
 
-    public function avisEnvoyes(): HasMany
+    public function avisEnvoyes()
     {
         return $this->hasMany(Avis::class, 'auteur_id');
     }
 
-    public function avisRecus(): HasMany
+    public function avisRecus()
     {
         return $this->hasMany(Avis::class, 'cible_id');
     }
